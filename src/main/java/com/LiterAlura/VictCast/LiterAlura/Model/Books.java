b9977ac8ccf61e22034615b914134fd99350cc4b
VictorCast2 <castillocastrovictorjose@hotmail.com>
2025-01-05 00:38:01 -0500

    Update

---
+++ src/main/java/com/LiterAlura/VictCast/LiterAlura/Model/Books.java
@@ -0,0 +1,14 @@
+package com.LiterAlura.VictCast.LiterAlura.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Books")
+public class Books {
+
+    
+
+}
